a0291e6c2f9e996a94950159c3dfb52fa4b79f3b
= <albertopmdm@gmail.com>
2022-04-24 16:08:50 -0500

    main only to import namesapaces for now

---
+++ main.py
@@ -0,0 +1,6 @@
+import logging
+from src.database.database import Foods, Ingredients
+from src.utils.utils import Utils, W
+from src.program.program import Foodlist
+
+logging.basicConfig(level=logging.NOTSET)
