1ad73f01c45a226c33e5a75f56bce396b3a6de54
= <albertopmdm@gmail.com>
2022-06-01 01:48:59 -0500

    main class for app

---
+++ app.py
@@ -0,0 +1,14 @@
+import tkinter as tk
+from src.app.main import Main
+
+class App(tk.Tk):
+
+    def __init__(self):
+        super().__init__()
+
+        self.geometry('1200x600')
+
+if __name__ == '__main__':
+    app = App()
+    Main(app)
+    app.mainloop()
