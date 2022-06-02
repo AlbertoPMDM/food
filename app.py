95fa3cb5ee174b02a73bbb690e6b07ee28d3c6ba
= <albertopmdm@gmail.com>
2022-06-02 10:51:27 -0500

    removed forced geometry

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ class App(tk.Tk):
     def __init__(self):
         super().__init__()
 
-        self.geometry('1200x600')
+        # self.geometry('1200x600')
 
 if __name__ == '__main__':
     app = App()
